7e85d19a75125fd564c5028d7355084534f739e9
Tony Grant <tony@sustainableartsfoundation.org>
2022-06-04 22:33:50 -0700

    updated PDFInfo to use self. duh.

--- lib/combine_pdf/pdf_info.rb
+++ lib/combine_pdf/pdf_info.rb
@@ -2,75 +2,75 @@ module CombinePDF
   class PDFInfo < Hash
 
     def title
-      @info[:Title]
+      self[:Title]
     end
 
     def title=(new_title = nil)
-      @info[:Title] = new_title
+      self[:Title] = new_title
     end
 
     def author
-      @info[:Author]
+      self[:Author]
     end
 
     def author=(new_author = nil)
-      @info[:Author] = new_author
+      self[:Author] = new_author
     end
 
     def subject
-      @info[:Subject]
+      self[:Subject]
     end
 
     def subject=(new_subject = nil)
-      @info[:Subject] = new_subject
+      self[:Subject] = new_subject
     end
 
     def keywords
-      @info[:Keywords]
+      self[:Keywords]
     end
 
     def keywords=(new_keywords = nil)
-      @info[:KeyWords] = new_keywords
+      self[:KeyWords] = new_keywords
     end
 
     def creator
-      @info[:Creator]
+      self[:Creator]
     end
 
     def creator=(new_creator = nil)
-      @info[:Creator] = new_creator
+      self[:Creator] = new_creator
     end
 
     def producer
-      @info[:Producer]
+      self[:Producer]
     end
 
     def producer=(new_producer = nil)
-      @info[:Producer] = new_producer
+      self[:Producer] = new_producer
     end
 
     def creation_date
-      @info[:CreationDate]
+      self[:CreationDate]
     end
 
     def creation_date=(new_creation_date = nil)
-      @info[:CreationDate] = new_creation_date
+      self[:CreationDate] = new_creation_date
     end
 
     def mod_date
-      @info[:ModDate]
+      self[:ModDate]
     end
 
     def mod_date=(new_mod_date = nil)
-      @info[:ModDate] = new_mod_date
+      self[:ModDate] = new_mod_date
     end
 
     def trapped
-      @info[:Trapped]
+      self[:Trapped]
     end
 
     def trapped=(new_trapped = nil)
-      @info[:Trapped] = new_trapped
+      self[:Trapped] = new_trapped
     end
 
   end
